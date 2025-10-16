9d81b08715c78a06644d3f2ed78ef65030c03d66
Mario Enrique <etalvarez03@gmail.com>
2025-10-16 17:16:43 -0600

    updating envs

--- generate-env.ts
+++ generate-env.ts
@@ -2,14 +2,13 @@ import { writeFileSync } from 'fs';
 
 const env = {
   production: true,
-  companyName: process.env.COMPANY_NAME,
-  companyName2: process.env.COMPANY_NAME2,
-  companySlogan: process.env.COMPANY_SLOGAN,
-  giphyApiKey: process.env.GIPHY_API_KEY,
-  giphyUrl: process.env.GIPHY_URL
+  companyName: process.env['COMPANY_NAME'],
+  companyName2: process.env['COMPANY_NAME2'],
+  companySlogan: process.env['COMPANY_SLOGAN'],
+  giphyApiKey: process.env['GIPHY_API_KEY'],
+  giphyUrl: process.env['GIPHY_URL']
 };
 
 const content = `export const environment = ${JSON.stringify(env, null, 2)};\n`;
 
 writeFileSync('./src/environments/environment.ts', content);
-
